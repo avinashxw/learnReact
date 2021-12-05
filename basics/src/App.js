2e2d2d69207fc25ccf2c3b30caa937398f4edd3b
Avinash Wadhawan <avinashxw@gmail.com>
2021-12-06 01:31:34 +0530

    create simple first compnent

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Let's get started!</h1>
+      <ExpenseItem></ExpenseItem>
       </header>
     </div>
-  );
+  ); 
 }
 
 export default App;
---
+++ basics/src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpenseItem() {
+    return (<div><h2>Expense Item(s)</h2><div>Date</div><div><h2>Title</h2><div>Amount</div></div></div>);
+}
+
+export default ExpenseItem;
